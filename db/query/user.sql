45b0d235457782f69d9cbe763c341912f9de145b
arthurlch <lachat.arthur74@gmail.com>
2023-08-10 17:09:50 +0900

    chores: remove old queries and add new one from the new schema model

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,28 +1,14 @@
--- user.sql
+-- Insert
+INSERT INTO "User" (name, email, phone, role) VALUES ($1, $2, $3, $4) RETURNING id;
 
--- name: CreateUser :one
-INSERT INTO "User" (
-  name,
-  email,
-  phone,
-  created_at,
-  updated_at
-) VALUES (
-  $1, -- name
-  $2, -- email
-  $3, -- phone
-  CURRENT_TIMESTAMP, -- created_at
-  CURRENT_TIMESTAMP -- updated_at
-) RETURNING id;
-
--- name: GetUsers :many
+-- Select
 SELECT * FROM "User";
 
--- name: UpdateUser :exec
-UPDATE "User"
-SET name = $1, email = $2, phone = $3, updated_at = CURRENT_TIMESTAMP
-WHERE id = $4; -- Specify the condition for updating, such as the "id" column
+-- Select a specific user by ID
+SELECT * FROM "User" WHERE id = $1;
 
--- name: DeleteUser :exec
-DELETE FROM "User"
-WHERE id = $1; -- Specify the condition for deletion, such as the "id" column
+-- Update
+UPDATE "User" SET name = $1, email = $2, phone = $3, role = $4, updated_at = CURRENT_TIMESTAMP WHERE id = $5;
+
+-- Delete
+DELETE FROM "User" WHERE id = $1;
